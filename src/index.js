c4649b735e3019f7612e3d859973083f8f65da9d
ElenaZhukouskaya <elenazhukouskaya@gmail.com>
2022-02-19 14:55:14 +0300

    feat: add function check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  let bracketsStr = bracketsConfig.join(",").split(",").join("");
+  let brackets = [];
+  for (let i = 0; i < bracketsStr.length; i += 2) {
+    brackets.push(bracketsStr.slice(i, i + 2));
+  }
+  for (let i = 0; i < brackets.length; i++) {
+    if (str.includes(brackets[i])) {
+      str = str.replace(brackets[i], "");
+      i = -1;
+    }
+  }
+  return str.length === 0;
+};
